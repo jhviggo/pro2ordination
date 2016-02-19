f32c370acda486798b55ecdeba7ee8c17d51b2a8
Margrethe Dybdahl <mad@eaaa.dk>
2016-02-19 15:38:11 +0100

    Service singleton med ekstra metode til at hente testService. Storage ikke længere static

--- Ordination/gui/MainApp.java
+++ Ordination/gui/MainApp.java
@@ -1,6 +1,5 @@
 package gui;
 
-import service.Service;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
@@ -8,54 +7,58 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import service.Service;
 
 public class MainApp extends Application {
+	private Service service;
 
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-    
-    @Override
-    public void init() {
-        Service.createSomeObjects();
-    }
-    
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Medicinordination");
-        BorderPane pane = new BorderPane();
-        this.initContent(pane);
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
 
-        Scene scene = new Scene(pane);
-        stage.setScene(scene);
-        stage.setHeight(500);
-        stage.setWidth(800);
-        stage.show();
-    }
-    
-    private void initContent(BorderPane pane) {
-        TabPane tabPane = new TabPane();
-        this.initTabPane(tabPane);
-        pane.setCenter(tabPane);
-    }
-    
-    private void initTabPane(TabPane tabPane) {
-        tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+	@Override
+	public void init() {
+		service = Service.getService();
+		service.createSomeObjects();
+	}
 
-        Tab tabOpret = new Tab("Opret ordinationer");
-        Tab tabVis = new Tab("Vis ordinationer");
-        Tab tabStatistik = new Tab("Vis statistik");
-        
-        tabOpret.setContent(new OpretOrdinationPane());
-        VisOrdinationPane visOrdinationPane = new VisOrdinationPane();
-        tabVis.setContent(visOrdinationPane);
-        tabStatistik.setContent(new StatistikPane());
-        
-        tabPane.getTabs().add(tabOpret);
-        tabPane.getTabs().add(tabVis);
-        tabPane.getTabs().add(tabStatistik);
+	@Override
+	public void start(Stage stage) {
+		stage.setTitle("Medicinordination");
+		BorderPane pane = new BorderPane();
+		this.initContent(pane);
 
-        tabVis.setOnSelectionChanged(event -> visOrdinationPane.updateControls());
-    }
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+		stage.setHeight(500);
+		stage.setWidth(800);
+		stage.show();
+	}
+
+	private void initContent(BorderPane pane) {
+		TabPane tabPane = new TabPane();
+		this.initTabPane(tabPane);
+		pane.setCenter(tabPane);
+	}
+
+	private void initTabPane(TabPane tabPane) {
+		tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+
+		Tab tabOpret = new Tab("Opret ordinationer");
+		Tab tabVis = new Tab("Vis ordinationer");
+		Tab tabStatistik = new Tab("Vis statistik");
+
+		tabOpret.setContent(new OpretOrdinationPane());
+		VisOrdinationPane visOrdinationPane = new VisOrdinationPane();
+		tabVis.setContent(visOrdinationPane);
+		tabStatistik.setContent(new StatistikPane());
+
+		tabPane.getTabs().add(tabOpret);
+		tabPane.getTabs().add(tabVis);
+		tabPane.getTabs().add(tabStatistik);
+
+		tabVis.setOnSelectionChanged(event -> visOrdinationPane
+				.updateControls());
+	}
 
 }
